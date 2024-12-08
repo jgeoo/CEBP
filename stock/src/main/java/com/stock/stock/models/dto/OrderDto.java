098552edf4682bba75e33286e5a188ecf3f5c989
vmo <vlad.mocan@dialogdata.de>
2024-12-08 23:44:03 +0200

    added cors config

---
+++ stock/src/main/java/com/stock/stock/models/dto/OrderDto.java
@@ -0,0 +1,17 @@
+package com.stock.stock.models.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class OrderDto {
+    private String company;
+    private int quantity;
+    private double price;
+    private boolean isBuyOrder;
+}
