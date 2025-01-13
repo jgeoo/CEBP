3e645fbc2a6cb01220c73ba9c2a14f6b520c4be3
vmo <vlad.mocan@dialogdata.de>
2025-01-13 12:36:20 +0200

    added log out functionality, and hid header when user is not logged in

--- cebp/src/components/Header.jsx
+++ cebp/src/components/Header.jsx
@@ -2,10 +2,26 @@ import {AppBar, Box, IconButton, Toolbar, Tooltip} from "@mui/material";
 import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
 import SellOutlinedIcon from '@mui/icons-material/SellOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import Logo from "../assets/logo-no-background.svg";
-import {Link} from "react-router";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 export default function Header() {
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogout = () => {
+        localStorage.clear(); // Clear local storage
+        setIsLoggedIn(false); // Update state
+        navigate("/"); // Navigate to the login page
+    };
+
+    useEffect(() => {
+        const id = localStorage.getItem("id");
+        setIsLoggedIn(!!id);
+    }, []);
+
     return (
         <Box sx={{}}>
         <AppBar
@@ -23,7 +39,7 @@ export default function Header() {
                     alignItems: "center",
                 }}
             >
-                <Link to='/'>
+                <Link to='/home'>
                     <img
                         src={Logo}
                         alt="Stock Exchange"
@@ -33,33 +49,39 @@ export default function Header() {
                     />
                 </Link>
 
-                <Box sx = {{display:'flex', justifyContent:'space-between', width:'10%'}}>
+                {isLoggedIn && (
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '10%' }}>
+                        <Link to='/sell-orders'>
+                            <Tooltip title="Sell Orders" arrow>
+                                <IconButton sx={{ color: '#333' }}>
+                                    <SellOutlinedIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </Link>
 
-                    <Link to='/sell-orders'>
-                        <Tooltip title="Sell Orders" arrow>
-                            <IconButton sx={{color: '#333'}}>
-                                <SellOutlinedIcon/>
+                        <Link to='/buy-orders'>
+                            <Tooltip title="Buy Orders" arrow>
+                                <IconButton sx={{ color: '#333' }}>
+                                    <ShoppingCartOutlinedIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </Link>
+
+                        <Link to='/transactions'>
+                            <Tooltip title="Transactions" arrow>
+                                <IconButton sx={{ color: '#333' }}>
+                                    <InventoryOutlinedIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </Link>
+
+                        <Tooltip title="Logout" arrow>
+                            <IconButton sx={{ color: '#333' }} onClick={handleLogout}>
+                                <LogoutOutlinedIcon />
                             </IconButton>
                         </Tooltip>
-                    </Link>
-
-                    <Link to ='/buy-orders'>
-                        <Tooltip title="Buy Orders" arrow>
-                            <IconButton sx ={{color:'#333'}}>
-                                <ShoppingCartOutlinedIcon />
-                            </IconButton>
-                        </Tooltip>
-                    </Link>
-
-                    <Link to ='/transactions'>
-                        <Tooltip title="Transactions" arrow>
-                            <IconButton sx ={{color:'#333'}}>
-                                <InventoryOutlinedIcon />
-                            </IconButton>
-                        </Tooltip>
-                    </Link>
-
-                </Box>
+                    </Box>
+                )}
             </Toolbar>
         </AppBar>
         </Box>
